7c2d0ca540cb5bda1036cde00105f9531a24eb31
cchalla1 <chandrakanth.cck@gmail.com>
2017-12-19 22:07:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var express = require("express"),
     app = express();
 
 app.use(express.static(__dirname + '/Client'));
-app.use(express.static(__dirname + '/views'));
+app.use(express.favicon(path.join(config.root, '/', 'favicon.ico')));
+app.use(express.static(path.join(config.root, '/')));
+app.set('views', config.root + '/views');
+// app.use(express.static(__dirname + '/views'));
 
 exports.db = mongoose.createConnection('mongodb://heroku_h4wb4tck:ornq8ql31cf3n6885b6ho0g05n@ds159866.mlab.com:59866/heroku_h4wb4tck');
 exports.autoIncrement = require('mongoose-auto-increment');
